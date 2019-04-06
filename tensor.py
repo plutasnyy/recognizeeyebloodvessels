ca011930d1675b03f76317756a8e020f13f18527
plutasnyy <kamil.plucinski97@gmail.com>
2019-04-06 16:34:57 +0200

    Created function for drawing list of images and added histogram equalization

--- tensor.py
+++ tensor.py
@@ -1,8 +1,13 @@
+from array import array
+
+from numpy import asarray
+
+
 class Tensor:
     def __init__(self, base_image, vessels):
         assert base_image.size == vessels.size, "Images have different sizes"
-        self.base_image = base_image
-        self.vessels = vessels
+        self.base_image = asarray(base_image)
+        self.vessels = asarray(vessels)
 
     def __repr__(self):
         return 'base_image: {}, vessels: {}, size: {}' \
