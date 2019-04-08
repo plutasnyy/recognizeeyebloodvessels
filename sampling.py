085a63cfe7511a0fb658ac9f7b554d3126ae0ef0
Artur Mierzwa <ashan22442@gmail.com>
2019-04-08 21:10:08 +0200

    sampling

--- sampling.py
+++ sampling.py
@@ -1,22 +1,21 @@
 import numpy as np
 from PIL import Image
+import glob
 
-array2 = []
-i=1
-im = Image.open('images/00'+str(i)+'.jpg')
-
-width, height = im.size
-im = np.array(im)
-image = Image.open('mask/00'+str(i)+'mask.jpg')
-
-image = np.array(image)
-
-for q in range(width-30):
-    for y in range(height-30):
-        print(q)
-        if image[y+16][q+16].all() == 0:
-            continue
-        else:
-            print([y,y+31],[q,q+31])
-            array2.append([[y,y+31],[q,q+31]])
-
+array = []
+w = 1
+images_path = sorted(glob.glob("images/*"))
+images_path2 = sorted(glob.glob("masks/*"))
+for i in range(1,186,2):
+    array2 = []
+    im = np.asarray(Image.open(images_path2[w]))
+    w+=1
+    image = np.asarray(Image.open(images_path[i]))
+    for x in range(len(im[0])-30):
+        for y in range(len(im)-30):
+            if image[y+16][x+16].all() == 0:
+                continue
+            else:
+                print([y,y+31],[x,x+31])
+                array2.append([[y,y+31],[x,x+31]])
+    array.append(array2)
