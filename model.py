468d87de502eabf4cc32df9a4d083eaf5ddf8b42
plutasnyy <kamil.plucinski97@gmail.com>
2019-04-20 10:59:12 +0200

    added predictions for model

--- model.py
+++ model.py
@@ -4,6 +4,10 @@ from keras import Sequential
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 
+import numpy as np
+
+from utils import PATCH_SIZE
+
 
 class Model:
     def __init__(self):
@@ -14,7 +18,7 @@ class Model:
         self.model.load_weights(filename)
 
     def fit(self, X, y):
-        filepath = "weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+        filepath = "weights/weights-{epoch:02d}-{val_acc:.4f}.hdf5"
         checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
         callbacks_list = [checkpoint]
         self.model.fit(X, y, validation_split=0.1, epochs=500, batch_size=10, callbacks=callbacks_list, verbose=1)
@@ -23,12 +27,27 @@ class Model:
         self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         logging.info('Compiled keras model')
 
+    def evaluate(self, X, y):
+        result = self.model.evaluate(X, y)
+        logging.info('Model evaluated, loss: {} acc: {}'.format(result[0], result[1]))
+        return result
+
+    def predict(self, X):
+        input = X
+        if len(input.shape) == 2:
+            input = input.reshape(1, PATCH_SIZE, PATCH_SIZE, 1)
+            logging.debug('Reshaped before prediction')
+        result = self.model.predict(input)
+        logging.info('Predicted: {}, return {}'.format(result, np.argmax(result)))
+        logging.debug('For: {}'.format(input))
+        return result
+
     @staticmethod
     def _build_model():
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3),
                          activation='relu',
-                         input_shape=(32, 32, 1)))
+                         input_shape=(PATCH_SIZE, PATCH_SIZE, 1)))
         model.add(Conv2D(64, (3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
--- utils.py
+++ utils.py
@@ -5,17 +5,22 @@ from PIL import Image
 
 from tensor import Tensor
 
+PATCH_SIZE = 32
+HALF_OF_PATCH_SIZE = int(PATCH_SIZE / 2)
+
 
 def create_tensor_from_file(one_tensor=False):
     logging.info('Create tensor list, only one tensor is {}'.format(one_tensor))
     tensors_list = list()
     images_path = sorted(glob.glob("images/*"))
     for i in range(0, len(images_path), 2):
+        # TODO FIX for i=164
         base_image = Image.open(images_path[i])
         vessels = Image.open(images_path[i + 1])
         file_name = images_path[i].replace('/', '.').split('.')[1]
         mask = Image.open('masks/{}.jpg'.format(file_name))
         tensors_list.append(Tensor(base_image, vessels, mask, i))
+        tensors_list[0].draw_tensor()
         if one_tensor:
             break
     return tensors_list
